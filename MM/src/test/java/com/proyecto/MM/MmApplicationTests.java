0dbb776a5d592f2e3e40f38300d0416727349bd0
MalcomMinissale <95878951+MalcomMinissale@users.noreply.github.com>
2022-07-31 06:28:00 -0300

    primer version de backend

---
+++ MM/src/test/java/com/proyecto/MM/MmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.MM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
